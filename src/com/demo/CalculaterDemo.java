98aa231e1281ad6dbd2bd08045596409cf55a8a0
Agr828 <yvsss81@gmail.com>
2021-03-09 12:49:07 +0530

    new1

--- src/com/demo/CalculaterDemo.java
+++ src/com/demo/CalculaterDemo.java
@@ -2,5 +2,7 @@ package com.demo;
 public class CalculaterDemo {
 public static void main(String[] args) {
 	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
 }
 }
